45da41a8234d038d0ff51f86963ef8fed88a66c9
lzl <lzl102872@163.com>
2019-09-07 23:43:34 +0800

    Initial respositories

---
+++ gh-page.sh
@@ -0,0 +1,21 @@
+#! /bin/sh
+
+rm -rf publish
+cd site
+
+node ../packages/nodeppt/bin/nodeppt build index.md
+node ../packages/nodeppt/bin/nodeppt build animation.md
+node ../packages/nodeppt/bin/nodeppt build component.md
+node ../packages/nodeppt/bin/nodeppt build layout.md
+node ../packages/nodeppt/bin/nodeppt build media.md
+node ../packages/nodeppt/bin/nodeppt build background.md
+
+cd dist
+
+git init
+git add -A
+date_str=`date "+DATE: %m/%d/%Y%nTIME: %H:%M:%S"`
+git commit -m "build with nodeppt on $date_str"
+#exit
+echo 'push remote github'
+git push -u git@github.com:Scorpio-li/nodeppt master:gh-pages --force
